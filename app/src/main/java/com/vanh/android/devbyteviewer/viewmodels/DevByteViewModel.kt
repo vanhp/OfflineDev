375c7cde2942312b5f582d291780eae41650895d
vanh <vanhphom@gmail.com>
2020-07-07 01:47:45 -0700

    hook up the repository pattern and using the DB as offline caching

--- app/src/main/java/com/vanh/android/devbyteviewer/viewmodels/DevByteViewModel.kt
+++ app/src/main/java/com/vanh/android/devbyteviewer/viewmodels/DevByteViewModel.kt
@@ -19,9 +19,11 @@ package com.vanh.android.devbyteviewer.viewmodels
 
 import android.app.Application
 import androidx.lifecycle.*
+import com.vanh.android.devbyteviewer.database.getDatabase
 import com.vanh.android.devbyteviewer.domain.Video
 import com.vanh.android.devbyteviewer.network.Network
 import com.vanh.android.devbyteviewer.network.asDomainModel
+import com.vanh.android.devbyteviewer.repository.VideosRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
@@ -40,54 +42,75 @@ import java.io.IOException
  */
 class DevByteViewModel(application: Application) : AndroidViewModel(application) {
 
+    private val database = getDatabase(application)
+    private val videoRepository = VideosRepository(database)
     /**
      * This is the job for all coroutines started by this ViewModel.
-     *
      * Cancelling this job will cancel all coroutines started by this ViewModel.
      */
     private val viewModelJob = SupervisorJob()
-
     /**
      * This is the main scope for all coroutines launched by MainViewModel.
-     *
      * Since we pass viewModelJob, you can cancel all coroutines launched by uiScope by calling
      * viewModelJob.cancel()
      */
     private val viewModelScope = CoroutineScope(viewModelJob + Dispatchers.Main)
-
-    /**
-     * A playlist of videos that can be shown on the screen. This is private to avoid exposing a
-     * way to set this value to observers.
-     */
-    private val _playlist = MutableLiveData<List<Video>>()
-
-    /**
-     * A playlist of videos that can be shown on the screen. Views should use this to get access
-     * to the data.
-     */
-    val playlist: LiveData<List<Video>>
-        get() = _playlist
-
-    /**
-     * init{} is called immediately when this ViewModel is created.
-     */
     init {
-        refreshDataFromNetwork()
-    }
-
-    /**
-     * Refresh data from network and pass it via LiveData. Use a coroutine launch to get to
-     * background thread.
-     */
-    private fun refreshDataFromNetwork() = viewModelScope.launch {
-        try {
-            val playlist = Network.devbytes.getPlaylist().await()
-            _playlist.postValue(playlist.asDomainModel())
-        } catch (networkError: IOException) {
-            // Show an infinite loading spinner if the request fails
-            // challenge exercise: show an error to the user if the network request fails
+            viewModelScope.launch{
+            videoRepository.refreshVideos()
         }
     }
+
+    val playlist = videoRepository.videos
+
+//    /**
+//     * A playlist of videos that can be shown on the screen. This is private to avoid exposing a
+//     * way to set this value to observers.
+//     */
+//    private val _playlist = MutableLiveData<List<Video>>()
+//
+//    /**
+//     * A playlist of videos that can be shown on the screen. Views should use this to get access
+//     * to the data.
+//     */
+//    val playlist: LiveData<List<Video>>
+//        get() = _playlist
+//
+//    /**
+//     * init{} is called immediately when this ViewModel is created.
+//     */
+//    init {
+//        refreshDataFromNetwork()
+//    }
+//
+//    /**
+//     * Refresh data from network and pass it via LiveData. Use a coroutine launch to get to
+//     * background thread.
+//     */
+//    private fun refreshDataFromNetwork() = viewModelScope.launch {
+//        try {
+//            val playlist = Network.devbytes.getPlaylist().await()
+//            _playlist.postValue(playlist.asDomainModel())
+//        } catch (networkError: IOException) {
+//            // Show an infinite loading spinner if the request fails
+//            // challenge exercise: show an error to the user if the network request fails
+//        }
+//    }
+//
+
+//
+//    /**
+//     * Factory for constructing DevByteViewModel with parameter
+//     */
+//    class Factory(val app: Application) : ViewModelProvider.Factory {
+//        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+//            if (modelClass.isAssignableFrom(DevByteViewModel::class.java)) {
+//                @Suppress("UNCHECKED_CAST")
+//                return DevByteViewModel(app) as T
+//            }
+//            throw IllegalArgumentException("Unable to construct viewmodel")
+//        }
+//    }
 
     /**
      * Cancel all coroutines when the ViewModel is cleared
@@ -95,18 +118,5 @@ class DevByteViewModel(application: Application) : AndroidViewModel(application)
     override fun onCleared() {
         super.onCleared()
         viewModelJob.cancel()
-    }
-
-    /**
-     * Factory for constructing DevByteViewModel with parameter
-     */
-    class Factory(val app: Application) : ViewModelProvider.Factory {
-        override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-            if (modelClass.isAssignableFrom(DevByteViewModel::class.java)) {
-                @Suppress("UNCHECKED_CAST")
-                return DevByteViewModel(app) as T
-            }
-            throw IllegalArgumentException("Unable to construct viewmodel")
-        }
     }
 }
